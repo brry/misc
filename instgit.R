6575e8d0b04b20724f1b1845f0eeadd18a827545
Berry Boessenkool <berry-b@gmx.de>
2015-12-10 21:12:07 +0100

    Update instgit.R
    instructons added in the beginning of the file

--- instgit.R
+++ instgit.R
@@ -1,4 +1,27 @@
 
+# Quickly install a package from github without having to install devtools
+# Berry Boessenkool, December 2015, feedback welcome at berryb@gmx.de
+
+# Tell your github users this:
+## source("https://raw.githubusercontent.com/brry/misc/master/instgit.R")
+## instgithub("brry/extremeStat")
+## library(extremeStat)
+
+# works only:
+# - for pure R package structure repositories
+# - from the master branch
+# - with read-write-remove permission at getwd()
+# - for package dependencies all listed in 'Imports' ('Depends' etc might be added later)
+# tested only:
+# - on windows 7 with R3.2.2
+
+# Note: devtools::install_github is much more extensive!
+# Note: drat is also much better than this quick hack.
+# http://dirk.eddelbuettel.com/code/drat.html
+# https://github.com/eddelbuettel/drat
+# http://eddelbuettel.github.io/drat/DratForPackageAuthors.html
+
+
 instgithub <- function(
    pk, # "user/package"
    cleanup=TRUE, # remove downloaded zipfile and folder with source code
@@ -24,12 +47,12 @@ if(cleanup)
   unlink(pkn, recursive=TRUE)
   unlink(paste0(pkn,".zip"))
   }
-}
+} #end of function
 
-# example test cases, work fine on windows 7 with current R3.2.2 + write permission at getwd:
+
+# Worked fine on my computer for:
 if(FALSE){
-instgithub(pk="brry/extremeStat")
-library(extremeStat)
 instgithub("talgalili/installr")
 instgithub("hadley/readxl")
+instgithub("mages/googleVis")
 }
